897a141d999e3343f3f7dc1ff89f9843deb40273
Burke-LJX <blandon3@gulls.salisbury.edu>
2025-01-28 14:20:00 -0500

    Added Both Mock Draft and Big Board databse compatability

--- scraper.py
+++ scraper.py
@@ -8,7 +8,13 @@ res.raise_for_status()
 initialSoup = BeautifulSoup(res.text, 'html.parser')
 #Finds all links that contain new mock drafts
 links = initialSoup.find_all('a', class_='link-container')
+import firebase_admin
+from firebase_admin import firestore
 
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
+mockCounter = 1
 for link in links:
     href = link.get('href')
     if href:
@@ -21,10 +27,14 @@ for link in links:
         #Gains Player info from player name tags
         players = innerSoup.find_all(class_='player-name player-name-bold')
         counter = 1
+        doc_ref = db.collection("Mock_Drafts").document(f"{mockCounter}MockDraft")
+        doc_ref.set({})
         for player in players:
             #Fetches name and outputs it with pick counter
             player_link = player.find('a')
             if player_link and counter < 33:
                 player_name = player_link.get_text(strip=True)
+                doc_ref.update({f"Pick_{counter}": player_name})
                 print(f"Pick {counter}: Player: {player_name}")
                 counter += 1
+    mockCounter += 1
